7cdc658031a2fcca0960125b733534b11168437c
dmp <hellodmp@163.com>
2016-06-15 13:21:26 +0800

    debug generator bugs

--- preprocess.py
+++ preprocess.py
@@ -14,8 +14,22 @@ def create_data():
     x_test = np.linspace(-20,20,100)
     x_test = np.array(x_test).reshape((len(x_test), 1))
     y_test = np.sin(x_test)+2*x_test
-    print x_test, y_test
+    # print x_test, y_test
     return x_train, y_train, x_test, y_test
+
+
+def batch_generator1(x_dataset, y_dataset, batch_size, ):
+    Xbatch = np.zeros(batch_size)
+    Ybatch = np.zeros(batch_size)
+    batch_idx = 0
+    while True:
+        for i in range(0, len(x_dataset)):
+            Xbatch[batch_idx] = x_dataset[i]
+            Ybatch[batch_idx] = y_dataset[i]
+            batch_idx += 1
+            if batch_idx == batch_size:
+                batch_idx = 0
+                yield (Xbatch, Ybatch)
 
 
 def get_data(train_path, test_path):
@@ -26,8 +40,20 @@ def get_data(train_path, test_path):
     test_file = h5py.File(test_path)
     x_test = test_file['features']
     y_test = test_file['doses']
-
     return x_train, y_train, x_test, y_test
+
+
+def batch_generator(feature_dataset, dose_dataset, batch_size):
+    Xbatch = np.zeros((batch_size, 9))
+    Ybatch = np.zeros(batch_size)
+    batch_idx = 0
+    for i in range(0,len(feature_dataset)):
+        Xbatch[batch_idx] = feature_dataset[i]
+        Ybatch[batch_idx] = dose_dataset[i]
+        batch_idx += 1
+        if batch_idx == batch_size:
+            batch_idx = 0
+            yield (Xbatch, Ybatch)
 
 
 
--- test.py
+++ test.py
@@ -6,52 +6,27 @@ import preprocess
 import numpy as np
 
 
-def batch_generator1(x_dataset, y_dataset, batch_size):
-    Xbatch = np.zeros(batch_size)
-    Ybatch = np.zeros(batch_size)
-    batch_idx = 0
-    for i in range(0,len(feature_dataset)):
-        Xbatch[batch_idx] = x_dataset[i]
-        Ybatch[batch_idx] = y_dataset[i]
-        batch_idx += 1
-        if batch_idx == batch_size:
-            batch_idx = 0
-            yield (Xbatch, Ybatch)
-
 def train(x_train, y_train, x_test, y_test):
     netModel = Model().create_model()
     netModel.compile(loss='mean_squared_error', optimizer="rmsprop", metrics=["accuracy"])
     # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     # model1.compile(loss='mean_squared_error', optimizer=sgd, metrics=["accuracy"])
     print "STARTING TRAINING"
-    #netModel.fit(x_train, y_train, nb_epoch=64, batch_size=20, verbose=0)
-    train_data_generator = batch_generator1(x_train, y_train, 10)
-    model.fit_generator(
+    # netModel.fit(x_train, y_train, nb_epoch=64, batch_size=20, verbose=0)
+    train_data_generator = preprocess.batch_generator1(x_train, y_train, 10)
+    netModel.fit_generator(
         generator=train_data_generator,
         samples_per_epoch=10,
-        nb_epoch=64)
+        nb_epoch=5000)
+
     out = netModel.predict(x_test, batch_size=10)
     fig, ax = plt.subplots()
     ax.plot(x_test, y_test,'r')
-    print y_test, out
+    # print y_test, out
     ax.plot(x_test, out, 'k--', lw=4)
     ax.set_xlabel('Measured')
     ax.set_ylabel('Predicted')
     plt.show()
-
-
-def batch_generator(feature_dataset, dose_dataset, batch_size):
-    Xbatch = np.zeros((batch_size, 9))
-    Ybatch = np.zeros(batch_size)
-    batch_idx = 0
-    for i in range(0,len(feature_dataset)):
-        Xbatch[batch_idx] = feature_dataset[i]
-        Ybatch[batch_idx] = dose_dataset[i]
-        batch_idx += 1
-        if batch_idx == batch_size:
-            batch_idx = 0
-            yield (Xbatch, Ybatch)
-
 
 
 def image_train(x_train, y_train, x_test, y_test):
@@ -61,7 +36,7 @@ def image_train(x_train, y_train, x_test, y_test):
     # model1.compile(loss='mean_squared_error', optimizer=sgd, metrics=["accuracy"])
     print "STARTING TRAINING"
     netModel.fit(x_train, y_train, nb_epoch=64, batch_size=20, verbose=0)
-    model.fit(data, label, batch_size=100,nb_epoch=10,shuffle=True,verbose=1,show_accuracy=True,validation_split=0.2)
+    # model.fit(data, label, batch_size=100,nb_epoch=10,shuffle=True,verbose=1,show_accuracy=True,validation_split=0.2)
 
 
 if __name__ == '__main__':
